bd9e6d6a8cff9bb756fa7b39eb4eabdf53ad1e1e
Nikita Shalimo <MegaFrostBall@gmail.com>
2022-05-13 18:18:22 +0300

    add main content for the initial state page

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,15 @@
 import Header from '../header/header';
+import BigGlass from '../../img/big-glass.svg'
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header/>
+      <div className='main-container'>
+        <img alt='' src={BigGlass}></img>
+        <p>Start with searching a GitHub user</p>
+      </div>
     </div>
   );
 }
