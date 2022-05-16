4a24de9bd6b97ce247d70b2a3371c7e65bd3ab61
Nikita Shalimo <MegaFrostBall@gmail.com>
2022-05-16 21:02:24 +0300

    output user information using github API

--- src/components/user/user.js
+++ src/components/user/user.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import UserData from "../userData/userData";
 
 const User = () => {
+
+    const [userInfo, setUserInfo] = useState({});
+
+    const infoAboutUser = (data) => {
+        setUserInfo({
+            login: data.login,
+            name: data.name,
+            avatar: data.avatar_url,
+            followers: data.followers,
+            following: data.following,
+            url: data.html_url,
+            reposUrl: data.repos_url
+        })
+    }
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/Shalimo')
+            .then(data => data.json())
+            .then(data => {
+                infoAboutUser(data)
+            })
+    })
     return (
-        <UserData/>
+        <UserData userInfo = {userInfo}/>
     )
 }
 
--- src/components/userData/userData.js
+++ src/components/userData/userData.js
@@ -1,23 +1,23 @@
 import React from "react";
 import './userData.css';
-import Avatar from "../../img/avatar.svg";
+// import Avatar from "../../img/avatar.svg";
 import Followers from "../../img/followers.svg";
 import Following from "../../img/following.svg";
 
-const UserData = () => {
+const UserData = ({userInfo}) => {
     return (
         <div className="data-container">
-            <img className="avatar" alt="" src={Avatar}></img>
-            <h1 className="username">Dan Abramov</h1>
-            <a href="#" className="userlink">gaearon</a>
+            <img className="avatar" alt="" src={userInfo.avatar}></img>
+            <h1 className="username">{userInfo.name}</h1>
+            <a href={userInfo.url} className="userlink">{userInfo.url}</a>
             <div className="follow-container">
                 <div className="followers">
                     <img className="icon" src={Followers} alt=""></img>
-                    65.8k followers
+                    {userInfo.followers} followers
                 </div>
                 <div className="following">
                     <img className="icon" src={Following} alt=""></img>
-                    171 following
+                    {userInfo.following} following
                 </div>
             </div>
         </div>
