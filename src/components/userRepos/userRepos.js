57a97ad6defcfd30fbe26ffb23fa9a550ff906c2
Nikita Shalimo <MegaFrostBall@gmail.com>
2022-05-19 02:47:38 +0300

    add message about empty list of repositories

--- src/components/userRepos/userRepos.js
+++ src/components/userRepos/userRepos.js
@@ -1,7 +1,18 @@
 import React from "react";
 import "./userRepos.css";
+import NotFound from "../../img/repos-not-found.svg"
 
 const UserRepos = ({reposInfo}) => {
+
+    if (reposInfo.length === 0) {
+        return (
+            <div className='empty-container'>
+                <img alt='' src={NotFound}></img>
+                <p>Repository list is empty</p>
+            </div>
+        )
+    }
+
     return reposInfo.map((repo) => {
         return (
             <div key={repo.name} className="repos-container">
