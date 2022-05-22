f3d29749052a490734d3c86d0e242d150dd86a00
Nikita Shalimo <MegaFrostBall@gmail.com>
2022-05-22 10:06:04 +0300

    some styles fixes in header

--- src/components/pagination/pagination.js
+++ src/components/pagination/pagination.js
@@ -18,7 +18,7 @@ const Pagination = ({ reposPerPage, countRepos, paginate, firstRepoIndex, lastRe
                         </svg>}
                         breakLabel="..."
                         nextLabel={<svg width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M1 1L6 6L1 11" stroke="#808080" stroke-width="2"/>
+                        <path d="M1 1L6 6L1 11" stroke="#808080" strokeWidth="2"/>
                         </svg>
                         }
                         onPageChange={paginate}
